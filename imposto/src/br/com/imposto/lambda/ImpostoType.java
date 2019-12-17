19af2038dfd41c38089d8c8d739ae5451f6f23f9
Igor Ferreira de Almeida <igor.almeida@mercadolivre.com>
2019-12-17 08:30:20 -0300

    Adicionando novo exemplo com lambda

--- imposto/src/br/com/imposto/lambda/ImpostoType.java
+++ imposto/src/br/com/imposto/lambda/ImpostoType.java
@@ -1,28 +1,26 @@
 package br.com.imposto.lambda;
 
+import java.util.function.BiFunction;
+
 public enum ImpostoType {
 
-    ICMS(0.1) {
-        @Override
-        public double calcula(double valor) {
-            return valor * this.getPercentual();
-        }
-    }, ISS(0.06) {
-        @Override
-        public double calcula(double valor) {
-            return valor * this.getPercentual();
-        }
-    };
+    ICMS(0.1, (percentual, valor) -> percentual * valor),
+    ISS(0.06, (percentual, valor) -> percentual * valor);
 
-    private final double percentual;
+    private final Double percentual;
 
-    ImpostoType(double percentual) {
+    private final BiFunction<Double, Double, Double> calc;
+
+    ImpostoType(Double percentual, BiFunction<Double, Double, Double> calc) {
         this.percentual = percentual;
+        this.calc = calc;
     }
 
-    public abstract double calcula(double valor);
+    public Double calcula(double valor) {
+        return calc.apply(this.percentual, valor);
+    }
 
-    public double getPercentual() {
+    public Double getPercentual() {
         return percentual;
     }
 }
